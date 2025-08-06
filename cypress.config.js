ac727e53cf8393267c8695d4bb35d99d7b6df000
vinithmax <vinithtorres@email.com>
2025-08-06 23:42:43 +0400

    new changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,47 +1,31 @@
-// const { defineConfig } = require("cypress");
-
-// module.exports = defineConfig({
-//   e2e: {
-//      baseUrl: 'https://www.automationexercise.com/' ,
-//      pageLoadTimeout: 10000, // 10 seconds
-//      defaultCommandTimeout:5000, 
-//     watchForFileChanges:false,// 20 seconds
-//     setupNodeEvents(on, config) {
-//       // implement node event listeners here
-//     },
-//   },
-// });
-
-
-
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://www.automationexercise.com/',
     setupNodeEvents(on, config) {
       // implement node event listeners here if needed
     },
-    baseUrl: 'https://www.automationexercise.com/', // or your actual URL
+    chromeWebSecurity: false,
+    viewportWidth: 1280,
+    viewportHeight: 800,
+    defaultCommandTimeout: 8000,
+    pageLoadTimeout: 60000,
+    requestTimeout: 10000,
+    responseTimeout: 10000,
+    watchForFileChanges: false,
   },
   video: false,
-    reporter: "mochawesome",
+  screenshotOnRunFailure: false,
+  reporter: 'mochawesome',
   reporterOptions: {
-    reportDir: "cypress/reports",
+    reportDir: 'cypress/reports',
     overwrite: false,
     html: true,
     json: false,
-  screenshotOnRunFailure: false,
-   watchForFileChanges:false,
-  chromeWebSecurity: false,
-  viewportWidth: 1280,
-  viewportHeight: 800,
-  defaultCommandTimeout: 8000,
-  pageLoadTimeout: 60000,
-  requestTimeout: 10000,
-  responseTimeout: 10000,
+  },
   env: {
     CYPRESS_CACHE_DISABLED: true,
     NODE_ENV: 'test',
-  },
+  }
 });
-
